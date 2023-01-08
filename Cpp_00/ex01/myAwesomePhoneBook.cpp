b3c75d133ee97ab381e8bf88ed15df0fb522a1ee
rodsmade <rodrigogregoldo@gmail.com>
2023-01-08 04:45:39 -0300

    creating new (single) contact from phonebook menu

--- Cpp_00/ex01/myAwesomePhoneBook.cpp
+++ Cpp_00/ex01/myAwesomePhoneBook.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+const int   PHONEBOOK_SIZE = 8;
+
 class Contact {
     public:
         std::string firstName;
@@ -20,51 +22,76 @@ class Contact {
                     this->phoneNumber = phoneNumber;
                     this->darkestSecret = darkestSecret;
         }
+
+        void    ListDetails() {
+            std::cout << this->firstName << " | "
+                        << this->lastName << " | "
+                        << this->nickname << " | "
+                        << this->phoneNumber << " | "
+                        << this->darkestSecret;
+        }
 };
+
+class PhoneBook {
+    public:
+        Contact allContacts[PHONEBOOK_SIZE];
+
+        void ListAllContacts() {
+            std::cout << "Index | First Name | Last Name | Nickname | Phone Number | Darkest Secret";
+
+            for (int i = 0; i < PHONEBOOK_SIZE; i++) {
+                std::cout << i << " | ";
+                allContacts[i].ListDetails();
+                std::cout << '\n';
+            }
+        }
+};
+
+std::string getUserInput(std::string prompt) {
+    std::string input;
+
+    do {
+        std::cout << prompt;
+        std::getline(std::cin, input);
+    } while (input.compare("") == 0);
+    return (input);
+}
 
 int main()
 {
     std::string user_input;
-    // bool pgm_exit = false;
+    bool pgm_exit = false;
 
     std::string contactFirstName;
     std::string contactLastName;
     std::string contactNickname;
     std::string contactPhoneNumber;
     std::string contactDarkestSecret;
+    std::string cleanLine;
     
-    std::cout << "Insert First Name: ";
-    std::getline(std::cin, contactFirstName);
-    std::cout << "Insert Last Name: ";
-    std::getline(std::cin, contactLastName);
-    std::cout << "Insert Nickname: ";
-    std::getline(std::cin, contactNickname);
-    std::cout << "Insert Phone Number: ";
-    std::getline(std::cin, contactPhoneNumber);
-    std::cout << "Insert Darkest Secret: ";
-    std::getline(std::cin, contactDarkestSecret);
 
-    Contact myContact(contactFirstName, contactLastName, contactNickname, contactPhoneNumber, contactDarkestSecret);
+    do {
+        std::cout << "Enter `ADD` to add a new contact; `SEARCH` to see details for a contact; or `EXIT` to exit program: ";
+        std::cin >> user_input;
 
-    std::cout << "Contato 1:\n";
-    std::cout << "firstName: " << myContact.firstName << '\n';
-    std::cout << "lastName: " << myContact.lastName << '\n';
-    std::cout << "nickname: " << myContact.nickname << '\n';
-    std::cout << "phoneNumber: " << myContact.phoneNumber << '\n';
-    std::cout << "darkestSecret: " << myContact.darkestSecret << '\n';
-    // do {
-    //     std::cout << "Enter ADD to add a new contact; SEARCH to see details for a contact; or EXIT to exit program: ";
-    //     std::cin >> user_input;
+        if (user_input.compare("ADD") == 0) {
+            std::getline(std::cin, cleanLine);
+            contactFirstName = getUserInput("Insert First Name: ");
+            contactLastName = getUserInput("Insert Last Name: ");
+            contactNickname = getUserInput("Insert Nickname: ");
+            contactPhoneNumber = getUserInput("Insert Phone Number: ");
+            contactDarkestSecret = getUserInput("Insert Darkest Secret: ");
 
-    //     if (user_input.compare("ADD") == 0) {
-    //         std::cout << "deu um ADD\n";
-    //     } else if (user_input.compare("SEARCH") == 0) {
-    //         std::cout << "deu um SEARCH\n";
-    //     } else if (user_input.compare("EXIT") == 0) {
-    //         std::cout << "deu um EXIT\n";
-    //         std::cout << "Exiting program . . .\n";
-    //         pgm_exit = true;
-    //     }
-    //     std::cout << '\n';
-    // } while (!pgm_exit);
+            Contact myContact(contactFirstName, contactLastName, contactNickname, contactPhoneNumber, contactDarkestSecret);
+
+            myContact.ListDetails();
+        } else if (user_input.compare("SEARCH") == 0) {
+            std::cout << "deu um SEARCH\n";
+        } else if (user_input.compare("EXIT") == 0) {
+            std::cout << "deu um EXIT\n";
+            std::cout << "Exiting program . . .\n";
+            pgm_exit = true;
+        }
+        std::cout << "\n\n";
+    } while (!pgm_exit);
 }

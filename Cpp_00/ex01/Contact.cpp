8744b79907222ccf708c401cfcb04bceaa363215
rodsmade <rodrigogregoldo@gmail.com>
2023-01-10 19:03:46 -0300

    removed commented code

--- Cpp_00/ex01/Contact.cpp
+++ Cpp_00/ex01/Contact.cpp
@@ -19,23 +19,18 @@ Contact::~Contact(){};
 
 void Contact::ListDetails()
 {
-	// std::cout << std::setw(20);
 	std::cout << "  First Name:\t\t";
 	std::cout << this->firstName;
 	std::cout << '\n';
-	// std::cout << std::setw(20);
 	std::cout << "  Last Name:\t\t";
 	std::cout << this->lastName;
 	std::cout << '\n';
-	// std::cout << std::setw(20);
 	std::cout << "  Nickname:\t\t";
 	std::cout << this->nickname;
 	std::cout << '\n';
-	// std::cout << std::setw(20);
 	std::cout << "  Phone Number:\t\t";
 	std::cout << this->phoneNumber;
 	std::cout << '\n';
-	// std::cout << std::setw(20);
 	std::cout << "  Darkest Secret:\t";
 	std::cout << this->darkestSecret;
 	std::cout << '\n';

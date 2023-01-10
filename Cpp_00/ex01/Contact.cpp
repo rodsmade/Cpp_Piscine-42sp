3a22e9a0bb2b2899c84bc73b62f025c732a04acf
rodsmade <rodrigogregoldo@gmail.com>
2023-01-10 20:26:38 -0300

    Using initialization list in constructor

--- Cpp_00/ex01/Contact.cpp
+++ Cpp_00/ex01/Contact.cpp
@@ -2,17 +2,9 @@
 
 Contact::Contact(){};
 
-Contact::Contact(std::string firstName,
-				 std::string lastName,
-				 std::string nickname,
-				 std::string phoneNumber,
-				 std::string darkestSecret)
+Contact::Contact(std::string firstName, std::string lastName, std::string nickname, std::string phoneNumber, std::string darkestSecret)
+	: firstName(firstName), lastName(lastName), nickname(nickname), phoneNumber(phoneNumber), darkestSecret(darkestSecret)
 {
-	this->firstName = firstName;
-	this->lastName = lastName;
-	this->nickname = nickname;
-	this->phoneNumber = phoneNumber;
-	this->darkestSecret = darkestSecret;
 }
 
 Contact::~Contact(){};

591d8bca85ee1e57a39e32c54ff51bc32d4b4d5a
rodsmade <rodrigogregoldo@gmail.com>
2023-01-19 19:37:09 -0300

    included functions in main, also created new valgrind rule in makefile

--- Cpp_01/ex00/main.cpp
+++ Cpp_01/ex00/main.cpp
@@ -1,10 +1,18 @@
 #include "Zombie.hpp"
 
+Zombie* newZombie( std::string name ) ;
+void randomChump( std::string name ) ;
+
 int main()
 {
-    dsahdkshadjkhasjkdhk h nao usou as funcoes auxiliares caraio
     Zombie aroldo = Zombie("Aroldo");
     Zombie *etoale = new Zombie("Etoale");
 
+    std::cout << "New Zombie:" << std::endl;
+    Zombie *alaor = newZombie("Alaor");
+    std::cout << "Random Chump" << std::endl;
+    randomChump("Randy");
+
     delete etoale;
+    delete alaor;
 }

ef65cf9c8d9d2cb7642a1aa0721b5e4ac895e201
rodsmade <rodrigogregoldo@gmail.com>
2023-01-19 22:24:49 -0300

    ex02 done

---
+++ Cpp_01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    std::string aString = "HI THIS IS BRAIN";
+    std::string *stringPTR = &aString;
+    std::string &stringREF = aString;
+    // se não inicializar a ref, o compilador reclama:  error: declaration of reference variable 'stringREF' requires an initializer
+
+    std::cout << "Address of aString: " << &aString << std::endl;
+    std::cout << "Address pointed to by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Address pointed to by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "Value of aString: " << aString << std::endl;
+    std::cout << "Value pointed by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed by stringREF: " << stringREF << std::endl;
+
+
+    return (0);
+}

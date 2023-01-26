3ea805c944f2b4a5d08147cf8db9efd545645068
rodsmade <rodrigogregoldo@gmail.com>
2023-01-25 22:35:59 -0300

    implemented inheritance of ClapTrap, overloaded constructors and implemented class-specific member function

---
+++ Cpp_03/ex01/ScavTrap.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+public:
+    // Constructors/Destructor
+    ScavTrap(void);
+    ScavTrap(std::string name);
+	ScavTrap(const ScavTrap &other);
+    ~ScavTrap(void);
+
+    // Member functions
+	void guardGate();
+};

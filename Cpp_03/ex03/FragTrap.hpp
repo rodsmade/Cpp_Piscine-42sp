74cd4e986e80bc8f29d6c4ae4b4de7361476167f
rodsmade <rodrigogregoldo@gmail.com>
2023-01-26 14:41:32 -0300

    merged with changes in previous exercise

--- Cpp_03/ex03/FragTrap.hpp
+++ Cpp_03/ex03/FragTrap.hpp
@@ -4,6 +4,9 @@
 
 class FragTrap : public ClapTrap {
    public:
+    using ClapTrap::_hitPoints;
+    using ClapTrap::_attackDamage;
+
     // Constructors/Destructor
     FragTrap(void);
     FragTrap(std::string name);
@@ -15,6 +18,9 @@ class FragTrap : public ClapTrap {
     FragTrap &operator=(const FragTrap &other);
 
     // Member functions
+    void attack(const std::string &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
     void highFivesGuys(void);
     void printStatus();
 };

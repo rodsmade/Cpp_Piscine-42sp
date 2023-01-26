03da8c4d6b483e04a78480fb68f31cf748ca8b51
rodsmade <rodrigogregoldo@gmail.com>
2023-01-25 23:17:02 -0300

    refactored printStatus

--- Cpp_03/ex01/ScavTrap.cpp
+++ Cpp_03/ex01/ScavTrap.cpp
@@ -44,3 +44,7 @@ ScavTrap &ScavTrap::operator=(const ScavTrap &other) {
 void ScavTrap::guardGate() {
     std::cout << "ScavTrap " << this->getName() << " has entered Gatekeeper Mode" << std::endl;
 };
+
+void ScavTrap::printStatus(void) {
+    std::cout << "ScavTrap " << this->getName() << " has " << this->getHitPoints() << " HP and " << this->getEnergyPoints() << " energy" << std::endl;
+};

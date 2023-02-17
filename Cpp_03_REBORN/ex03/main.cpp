0cd2538707107dab5d4fa0d70ce412e6a07f5522
rodsmade <rodrigogregoldo@gmail.com>
2023-02-17 18:54:28 -0300

    incorporated all tests in diamond's main and adding -Wshadow flag to compilation

--- Cpp_03_REBORN/ex03/main.cpp
+++ Cpp_03_REBORN/ex03/main.cpp
@@ -5,75 +5,75 @@
 #include <cassert>
 
 int main() {
-    // {
-    //     std::cout << std::endl << "========================================= ClapTrap Tests ==========" << std::endl << std::endl;
-    //     ClapTrap clepiClepi = ClapTrap("Crépe-crépe");
-    //     clepiClepi.print_status();
-    //     clepiClepi.attack("Rihanna");
-    //     clepiClepi.attack("Rihanna");
-    //     clepiClepi.attack("Rihanna");
-    //     clepiClepi.attack("Rihanna");
-    //     clepiClepi.attack("Rihanna");
-    //     clepiClepi.beRepaired(1);
-    //     clepiClepi.beRepaired(1);
-    //     clepiClepi.beRepaired(1);
-    //     clepiClepi.beRepaired(1);
-    //     clepiClepi.beRepaired(1);
-    //     clepiClepi.attack("Rihanna");
-    //     clepiClepi.print_status();
+    {
+        std::cout << std::endl << "========================================= ClapTrap Tests ==========" << std::endl << std::endl;
+        ClapTrap clepiClepi = ClapTrap("Crépe-crépe");
+        clepiClepi.print_status();
+        clepiClepi.attack("Rihanna");
+        clepiClepi.attack("Rihanna");
+        clepiClepi.attack("Rihanna");
+        clepiClepi.attack("Rihanna");
+        clepiClepi.attack("Rihanna");
+        clepiClepi.beRepaired(1);
+        clepiClepi.beRepaired(1);
+        clepiClepi.beRepaired(1);
+        clepiClepi.beRepaired(1);
+        clepiClepi.beRepaired(1);
+        clepiClepi.attack("Rihanna");
+        clepiClepi.print_status();
 
-    //     ClapTrap clepiB = clepiClepi;  // same as ClapTrap clepiB = ClapTrap(clepiClepi);
-    //     clepiB.set_name("Clepi B");
-    //     clepiB.set_energy_points(3);
-    //     clepiB.print_status();
-    //     clepiClepi.print_status();
+        ClapTrap clepiB = clepiClepi;  // same as ClapTrap clepiB = ClapTrap(clepiClepi);
+        clepiB.set_name("Clepi B");
+        clepiB.set_energy_points(3);
+        clepiB.print_status();
+        clepiClepi.print_status();
 
-    //     ClapTrap clepiC;  // same as ClapTrap clepiC = ClapTrap();
-    //     clepiC = clepiB;
-    //     clepiC.set_name("Clépi C");
-    //     clepiC.set_attack_damage(9999);
-    //     clepiC.print_status();
-    //     clepiB.print_status();
-    //     clepiClepi.print_status();
-    // }
-    // {
-    //     std::cout << std::endl << "========================================= ScavTrap Tests ==========" << std::endl << std::endl;
-    //     ClapTrap *base_ptr;speedy
+        ClapTrap clepiC;  // same as ClapTrap clepiC = ClapTrap();
+        clepiC = clepiB;
+        clepiC.set_name("Clépi C");
+        clepiC.set_attack_damage(9999);
+        clepiC.print_status();
+        clepiB.print_status();
+        clepiClepi.print_status();
+    }
+    {
+        std::cout << std::endl << "========================================= ScavTrap Tests ==========" << std::endl << std::endl;
+        ClapTrap *base_ptr;
 
-    //     ClapTrap clepiClepi = ClapTrap("Crépe-crépe");
-    //     base_ptr = &clepiClepi;
-    //     clepiClepi.print_status();
-    //     base_ptr = &clepiClepi;
-    //     base_ptr->print_status();
+        ClapTrap clepiClepi = ClapTrap("Crépe-crépe");
+        base_ptr = &clepiClepi;
+        clepiClepi.print_status();
+        base_ptr = &clepiClepi;
+        base_ptr->print_status();
 
-    //     ScavTrap escave = ScavTrap("Escave");
-    //     escave.print_status();
-    //     base_ptr = &escave;
-    //     base_ptr->print_status();
-    //     escave.guardGate();
-    // }
-    // {
-    //     std::cout << std::endl << "========================================= FragTrap Tests ==========" << std::endl << std::endl;
-    //     ClapTrap *base_ptr;
+        ScavTrap escave = ScavTrap("Escave");
+        escave.print_status();
+        base_ptr = &escave;
+        base_ptr->print_status();
+        escave.guardGate();
+    }
+    {
+        std::cout << std::endl << "========================================= FragTrap Tests ==========" << std::endl << std::endl;
+        ClapTrap *base_ptr;
 
-    //     ClapTrap clepiClepi = ClapTrap("Crépe-crépe");
-    //     base_ptr = &clepiClepi;
-    //     // clepiClepi.print_status();
-    //     base_ptr = &clepiClepi;
-    //     // base_ptr->print_status();
+        ClapTrap clepiClepi = ClapTrap("Crépe-crépe");
+        base_ptr = &clepiClepi;
+        // clepiClepi.print_status();
+        base_ptr = &clepiClepi;
+        // base_ptr->print_status();
 
-    //     FragTrap speedy = FragTrap("Speedy Fragger");
-    //     // speedy.print_status();
-    //     base_ptr = &speedy;
-    //     // base_ptr->print_status();
-    //     speedy.highFivesGuys();
+        FragTrap speedy = FragTrap("Speedy Fragger");
+        // speedy.print_status();
+        base_ptr = &speedy;
+        // base_ptr->print_status();
+        speedy.highFivesGuys();
 
-    //     speedy.attack("Crépe-crépe");
-    //     clepiClepi.takeDamage(speedy.get_attack_damage());
-    //     clepiClepi.beRepaired(10);
-    //     // speedy.print_status();
-    //     // clepiClepi.print_status();
-    // }
+        speedy.attack("Crépe-crépe");
+        clepiClepi.takeDamage(speedy.get_attack_damage());
+        clepiClepi.beRepaired(10);
+        // speedy.print_status();
+        // clepiClepi.print_status();
+    }
     {
         DiamondTrap rihanna("Badgal Riri");
         FragTrap speedy("Speedy Fragger");

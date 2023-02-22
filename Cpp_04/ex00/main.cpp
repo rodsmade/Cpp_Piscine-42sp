7eb8506645ce0e5819d5fe3120a318563ad4f5b8
rodsmade <rodrigogregoldo@gmail.com>
2023-02-22 19:47:33 -0300

    incorporating my own tests to subject's mandatory

--- Cpp_04/ex00/main.cpp
+++ Cpp_04/ex00/main.cpp
@@ -1,8 +1,10 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main() {
     // Pdf tests:
+    std::cout << "Pdf tests:" << std::endl;
     const Animal* ptrToAnimal = new Animal();
     const Animal* ptrToDog = new Dog();
     const Animal* ptrToCat = new Cat();
@@ -11,6 +13,26 @@ int main() {
     ptrToCat->makeSound();  // will output the cat sound!
     ptrToDog->makeSound();
     ptrToAnimal->makeSound();
+    std::cout << std::endl;
+
+    // My tests:
+    std::cout << "My tests:" << std::endl;
+    std::cout << "Method calling by pointer:" << std::endl;
+    // If I use a base class pointer to point to an inherited class, the pointer will go for the base class' definition of the function (bc there's no 'virtual' declaration there)
+    const WrongAnimal* ptrToWrongAnimal = new WrongAnimal();
+    const WrongAnimal* ptrToWrongCat = new WrongCat();
+    ptrToWrongCat->makeSound();
+    ptrToWrongAnimal->makeSound();
+
+    std::cout << "Method calling by object:" << std::endl;
+    // However, if I simply instantiate the objects, because I'm explicitly and directly calling the inherited class' object, no confusion takes place.
+    const WrongAnimal wrongAnimalObj = WrongAnimal();
+    const WrongCat wrongCatObj = WrongCat();
+    wrongCatObj.makeSound();
+    wrongAnimalObj.makeSound();
+
+    std::cout << "WrongCat says: my type is " << wrongCatObj.getType() << std::endl;
+    std::cout << "WrongCat says: my base class' type is " << wrongCatObj.WrongAnimal::getType() << std::endl;
 
     return 0;
 }

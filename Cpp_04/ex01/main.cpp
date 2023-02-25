1d0f5cea37170ddae55763e1683a3e9d2efd629a
rodsmade <rodrigogregoldo@gmail.com>
2023-02-24 21:10:09 -0300

    improved tests for exercise

--- Cpp_04/ex01/main.cpp
+++ Cpp_04/ex01/main.cpp
@@ -10,11 +10,32 @@ int main()
     {
         // Mandatory tests
         std::cout << "===================================================== Mandatory tests:" << std::endl;
-        const Animal* dogPtr = new Dog();
-        const Animal* catPtr = new Cat();
+        {
+            std::cout << "--- Basic Constructor and Destructor test ---" << std::endl;
+            const Animal* dogPtr = new Dog();
+            const Animal* catPtr = new Cat();
 
-        delete dogPtr;//should not create a leak
-        delete catPtr;
+            delete dogPtr;//should not create a leak
+            delete catPtr;
+        }
+        {
+            std::cout << std::endl;
+            std::cout << "--- Array test ---" << std::endl;
+            std::cout << "... creating array ..." << std::endl;
+            Animal *animals[10];
+            for (int i = 0; i < 10; i++) {
+                if (i % 2) {
+                    animals[i] = new Dog();
+                } else {
+                    animals[i] = new Cat();
+                }
+            }
+            std::cout << "... destroying array ..." << std::endl;
+            for (int i = 0; i < 10; i++) {
+                delete animals[i];
+            }
+        }
+
         std::cout << std::endl;
     }
     {
@@ -52,7 +73,6 @@ int main()
         std::cout << "--- Destroying a Cat object: ---" << std::endl;
         delete catPtr;
 
-
         std::cout << std::endl;
         std::cout << "--- Copying a Cat object: ---" << std::endl;
         std::string newType = "Persian";
@@ -63,8 +83,32 @@ int main()
         cat2.setType(newType);
         std::cout << cat1.getType() << " is the type of cat1" << std::endl;
         std::cout << cat2.getType() << " is the type of cat2" << std::endl;
-        return 0;
-
     }
-return 0;
+    {
+        std::cout << std::endl;
+        std::cout << "===================================================== Surprise, surprise!" << std::endl;
+        Dog doggo;
+        {
+            Dog temp = doggo;
+            temp.setIdea(0, "Wag my lil' tail at all times");
+            doggo.setIdea(0, "Wag my lil' tail at all times");
+            temp.setIdea(1, "Suss out everything with my lil' snoot");
+            std::cout << "0th idea in my brain: " << temp.getIdea(0) << std::endl;
+            std::cout << "1st idea in my brain: " << temp.getIdea(1) << std::endl;
+        }
+        std::cout << "0th idea in my brain: " << doggo.getIdea(0) << std::endl;
+        std::cout << "1st idea in my brain: " << doggo.getIdea(1) << std::endl;
+        Cat purrBall;
+        {
+            Cat temp = purrBall;
+            temp.setIdea(0, "Sniff toes and look astonished");
+            purrBall.setIdea(0, "Sniff toes and look astonished");
+            temp.setIdea(1, "Tip every glass I come across");
+            std::cout << "0th idea in my brain: " << temp.getIdea(0) << std::endl;
+            std::cout << "1st idea in my brain: " << temp.getIdea(1) << std::endl;
+        }
+        std::cout << "0th idea in my brain: " << purrBall.getIdea(0) << std::endl;
+        std::cout << "1st idea in my brain: " << purrBall.getIdea(1) << std::endl;
+    }
+    return 0;
 }

79dd7ba2643e85ab3087d5f2906018f333e4fe32
rodsmade <rodrigogregoldo@gmail.com>
2023-03-02 16:43:03 -0300

    Bureaucrat class fully implemented

--- Cpp_05/ex02/Bureaucrat.hpp
+++ Cpp_05/ex02/Bureaucrat.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <exception>
 #include <iostream>
 
 #include "AForm.hpp"
@@ -12,6 +13,42 @@ class Bureaucrat {
     std::string _name;
 
    public:
+    // Constructors/Destructor
+    Bureaucrat(const std::string &name, int grade);
+    Bureaucrat(const Bureaucrat &other);
+    ~Bureaucrat();
+
+    // Accessors
     int getGrade() const;
+    std::string getName() const;
+
+    // Operator Overloads
+    // -- Assignment operator
+    Bureaucrat &operator=(const Bureaucrat &other);
+
+    // -- Increment/Decrement operators
+    Bureaucrat &operator++();    // Prefix increment operator.
+    Bureaucrat operator++(int);  // Postfix increment operator.
+    Bureaucrat &operator--();    // Prefix decrement operator.
+    Bureaucrat operator--(int);  // Postfix decrement operator.
+
+    // Member functions
     void signForm(AForm &form);
+
+    // Exceptions
+    class GradeTooHighException : public std::exception {
+       public:
+        virtual const char *what() const throw() {
+            return "Grade is too high.";
+        }
+    };
+
+    class GradeTooLowException : public std::exception {
+       public:
+        virtual const char *what() const throw() {
+            return "Grade is too low.";
+        }
+    };
 };
+
+// Output stream operator overload

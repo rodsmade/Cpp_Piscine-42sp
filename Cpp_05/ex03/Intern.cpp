1f9161a6564b073fbcf93b38e53670cd3af78679
rodsmade <rodrigogregoldo@gmail.com>
2023-03-03 18:56:16 -0300

    fixed if forest DONE

--- Cpp_05/ex03/Intern.cpp
+++ Cpp_05/ex03/Intern.cpp
@@ -13,16 +13,42 @@ Intern &Intern::operator=(const Intern &other) {
     return *this;
 };
 
-AForm *Intern::makeForm(std::string formName, std::string target) {
-    if (formName == "Presidential Pardon") {
-        std::cout << "Intern creates " << formName << " form." << std::endl;
+AForm *createPresidentialPardonForm(std::string name, std::string target) {
+    if (name == "Presidential Pardon") {
+        std::cout << "Intern creates " << name << " Form." << std::endl;
         return new PresidentialPardonForm(target);
-    } else if (formName == "Robotomy Request") {
-        std::cout << "Intern creates " << formName << " form." << std::endl;
+    }
+    else
+        return NULL;
+};
+
+AForm *createRobotomyRequestForm(std::string name, std::string target) {
+    if (name == "Robotomy Request") {
+        std::cout << "Intern creates " << name << " Form." << std::endl;
         return new RobotomyRequestForm(target);
-    } else if (formName == "Shrubbery Creation") {
-        std::cout << "Intern creates " << formName << " form." << std::endl;
+    }
+    else
+        return NULL;
+};
+
+AForm *createShrubberyCreationForm(std::string name, std::string target) {
+    if (name == "Shrubbery Creation") {
+        std::cout << "Intern creates " << name << " Form." << std::endl;
         return new ShrubberyCreationForm(target);
+    }
+    else
+        return NULL;
+};
+
+
+AForm *Intern::makeForm(std::string formName, std::string target) {
+    AForm *(*creationFunctions[3])(std::string name, std::string target) = {createPresidentialPardonForm, createRobotomyRequestForm, createShrubberyCreationForm};
+
+    AForm *result;
+    for (int i = 0; i < 3; i++) {
+        result = creationFunctions[i](formName, target);
+        if (result)
+            return result;
     }
     std::cout << "NO FORM CREATED BECAUSE THIS TYPE OF FORM DOES NOT EXIST HELLO" << std::endl;
     return NULL;

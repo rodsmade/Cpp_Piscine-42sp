9495637a09ced93ee5aeb3e6300d4706896fb76b
rodsmade <rodrigogregoldo@gmail.com>
2023-03-08 14:52:59 -0300

    wip: can recognize if characters are printable

--- Cpp_06/ex00/main.cpp
+++ Cpp_06/ex00/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cout << "Provide exactly one argument, e.g.: ./convert 42.0f" << std::endl;
-        return -42;
-    }
+	if (argc != 2) {
+		std::cout << "Provide exactly one argument, e.g.: ./convert 42.0f" << std::endl;
+		return -42;
+	}
 
-    std::cout << argv[0] << " " << argv[1] << std::endl;
+	// std::cout << argv[0] << " " << argv[1] << std::endl;
 
-    return 0;
+	int i = -1;
+	while (argv[1][++i])
+		std::cout << (std::isprint(argv[1][i]) == 0 ? "false" : "true") << std::endl;
+	
+	return 0;
 }

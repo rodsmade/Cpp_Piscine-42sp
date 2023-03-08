14b0f86e4a5a2566aa212f94e1f02ccff871ec72
rodsmade <rodrigogregoldo@gmail.com>
2023-03-08 15:14:42 -0300

    basic input validation OK

--- Cpp_06/ex00/main.cpp
+++ Cpp_06/ex00/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 
+bool does_not_have_exactly_one_argument(int argc) {
+    return (argc != 2);
+}
+
+bool has_unprintable_characters(char *arg) {
+    int i = -1;
+
+    while (arg[++i])
+        if (std::isprint(arg[i]) == 0)
+            return (true);
+    return (false);
+}
+
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		std::cout << "Provide exactly one argument, e.g.: ./convert 42.0f" << std::endl;
-		return -42;
-	}
+    if (does_not_have_exactly_one_argument(argc)) {
+        std::cout << "Provide exactly one argument, e.g.: ./convert 42.0f" << std::endl;
+        return (-42);
+    }
+    if (has_unprintable_characters(argv[1])) {
+        std::cout << "Argument has unprintable character in it, come on!" << std::endl;
+        return (-42);
+    }
+    
 
-	// std::cout << argv[0] << " " << argv[1] << std::endl;
 
-	int i = -1;
-	while (argv[1][++i])
-		std::cout << (std::isprint(argv[1][i]) == 0 ? "false" : "true") << std::endl;
-	
-	return 0;
+    return 0;
 }

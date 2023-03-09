dc93dc30f1db967a8172efa7cdfba7d4c89c7c53
rodsmade <rodrigogregoldo@gmail.com>
2023-03-09 13:02:48 -0300

    implemented conversion from char to int and back again, and printing messages seem fine too

--- Cpp_06/ex00/ConvertedNumber.cpp
+++ Cpp_06/ex00/ConvertedNumber.cpp
@@ -1,23 +1,33 @@
 #include "ConvertedNumber.hpp"
 
 ConvertedNumber::ConvertedNumber(std::string originalType, std::string arg) : _originalType(originalType) {
-    if (originalType == "float") {
-        // pode ser nanf !
-        this->_toFloat = std::atof(arg.c_str());
-        this->_toDouble = static_cast<double>(this->_toFloat);
-        this->_toChar = static_cast<char>(this->_toFloat);
-    }
-    if (originalType == "double") {
-        // pode ser nan !
-        this->_toDouble = std::strtod(arg.c_str(), NULL);
-        this->_toFloat = static_cast<float>(this->_toDouble);
-        this->_toChar = static_cast<char>(this->_toDouble);
-    }
     if (originalType == "char") {
-        this->_toChar = arg[0];
-        this->_toDouble = static_cast<double>(this->_toChar);
-        this->_toFloat = static_cast<float>(this->_toChar);
+        this->_char = arg[0];
+
+        this->_int = static_cast<int>(this->_char);
+        this->_double = static_cast<double>(this->_char);
+        this->_float = static_cast<float>(this->_char);
     }
+    if (originalType == "int") {
+        int temp = std::atoi(arg.c_str());
+
+        this->_int = static_cast<int>(temp);
+        this->_char = static_cast<char>(temp);
+        this->_float = static_cast<float>(temp);
+        this->_double = static_cast<double>(temp);
+    }
+    // if (originalType == "float") {
+    //     // pode ser nanf !
+    //     this->_float = std::atof(arg.c_str());
+    //     this->_double = static_cast<double>(this->_float);
+    //     this->_char = static_cast<char>(this->_float);
+    // }
+    // if (originalType == "double") {
+    //     // pode ser nan !
+    //     this->_double = std::strtod(arg.c_str(), NULL);
+    //     this->_float = static_cast<float>(this->_double);
+    //     this->_char = static_cast<char>(this->_double);
+    // }
 };
 
 // ConvertedNumber::ConvertedNumber(const ConvertedNumber &other){};
@@ -30,18 +40,41 @@ void convertToFloat();
 void convertToDouble();
 void convertToChar();
 
-void ConvertedNumber::printNumberInAllFormats(void){
+bool ConvertedNumber::charAndIntConversionIsImpossible() {
+    return (isnan(this->_double) || isnanf(this->_float) || isinf(this->_double) || isinff(this->_float));
+};
+
+void ConvertedNumber::printNumberInAllFormats(void) {
     // float f = std::atof(argv[1]);
     // // Set the number of decimal places to 1 and enable fixed-point notation
     // std::cout << std::fixed << std::setprecision(1);
     // // Print the float with a decimal point and trailing zeros
     // std::cout << "The float value is: " << f << "f" << std::endl;
     // CHAR
-    std::cout << "char: " << this->_toChar << std::endl;
+    std::cout << "char: ";
+    if (this->charAndIntConversionIsImpossible()) {
+        std::cout << "impossible";
+    }
+    if (!std::isprint(this->_char)) {
+        std::cout << "Non displayable";
+    } else {
+        std::cout << this->_char;
+    }
+    std::cout << std::endl;
+
     // INT
-    std::cout << "int: " << "TBA" << std::endl;
+    std::cout << "int: ";
+    if (this->charAndIntConversionIsImpossible()) {
+        std::cout << "impossible";
+    } else {
+        std::cout << this->_int;
+    }
+    std::cout << std::endl;
+
     // FLOAT
-    std::cout << "float: " << this->_toFloat << std::endl;
+    std::cout << "float: "
+              << "TBA" << std::endl;
     // DOUBLE
-    std::cout << "double: " << this->_toDouble << std::endl;
+    std::cout << "double: "
+              << "TBA" << std::endl;
 };
--- Cpp_06/ex00/ConvertedNumber.hpp
+++ Cpp_06/ex00/ConvertedNumber.hpp
@@ -1,13 +1,17 @@
 #include <cstdlib>
+#include <exception>
 #include <iostream>
+#include <limits.h>
+#include <cmath>
 #include <string>
 
 class ConvertedNumber {
    private:
     std::string _originalType;
-    float _toFloat;
-    double _toDouble;
-    char _toChar;
+    char _char;
+    int _int;
+    float _float;
+    double _double;
 
    public:
     ConvertedNumber(std::string originalType, std::string arg);
@@ -15,11 +19,19 @@ class ConvertedNumber {
     ~ConvertedNumber();
     // ConvertedNumber &operator=(const ConvertedNumber &other);
 
-    static float convertToFloat();
-    static double convertToDouble();
-    static char convertToChar();
-    std::string floatToString();
-    std::string doubleToString();
+    // static float convertToFloat();
+    // static double convertToDouble();
+    // static char convertToChar();
+    // std::string floatToString();
+    // std::string doubleToString();
 
-	void printNumberInAllFormats(void);
+    bool charAndIntConversionIsImpossible(void);
+    void printNumberInAllFormats(void);
+
+    class IntOutOfBoundsException : public std::exception {
+       public:
+        virtual const char *what() const throw() {
+            return "Number is greater than INT_MAX or smaller than INT_MIN.";
+        }
+    };
 };

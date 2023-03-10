fc0c17cc11a45cf1e2c6b1e880fcfc660ce3b6b8
rodsmade <rodrigogregoldo@gmail.com>
2023-03-10 16:36:08 -0300

    fixed tests, now the copy constructor can be correctly assessed

--- Cpp_07/ex02/main.cpp
+++ Cpp_07/ex02/main.cpp
@@ -3,8 +3,12 @@
 #include "Array.hpp"
 
 int main() {
+
+    Array<int> emptyArr;
+
     Array<int> myArr(5);
 
+    std::cout << "myArr is " << myArr.size() << " elements long" << std::endl;
     for (unsigned int i = 0; i < myArr.size(); i++)
         std::cout << "myArr[" << i << "]: " << myArr[i] << std::endl;
 
@@ -14,16 +18,16 @@ int main() {
         std::cout << e.what() << std::endl;
     }
 
-    std::cout << "myArr is " << myArr.size() << " elements long" << std::endl;
-
     Array<int> copy(myArr);
 
     for (unsigned int i = 0; i < copy.size(); i++)
-        copy[i] = 2;
+        copy[i] = 42;
 
+    std::cout << "copy is " << copy.size() << " elements long" << std::endl;
+    for (unsigned int i = 0; i < copy.size(); i++)
+        std::cout << "copy[" << i << "]: " << copy[i] << std::endl;
     for (unsigned int i = 0; i < myArr.size(); i++)
-        std::cout << "copy[" << i << "]: " << myArr[i] << std::endl;
-    std::cout << "copy is " << myArr.size() << " elements long" << std::endl;
+        std::cout << "myArr[" << i << "]: " << myArr[i] << std::endl;
 
     return 0;
 }

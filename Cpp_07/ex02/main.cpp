24e0a27969374509fd263daeb4db2678cb7f7cee
rodsmade <rodrigogregoldo@gmail.com>
2023-03-10 16:40:25 -0300

    wrote full tests for copy and assignment operators

--- Cpp_07/ex02/main.cpp
+++ Cpp_07/ex02/main.cpp
@@ -3,20 +3,12 @@
 #include "Array.hpp"
 
 int main() {
-
-    Array<int> emptyArr;
-
     Array<int> myArr(5);
 
     std::cout << "myArr is " << myArr.size() << " elements long" << std::endl;
     for (unsigned int i = 0; i < myArr.size(); i++)
         std::cout << "myArr[" << i << "]: " << myArr[i] << std::endl;
-
-    try {
-        std::cout << "[5]: " << myArr[5] << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
+    std::cout << std::endl;
 
     Array<int> copy(myArr);
 
@@ -28,6 +20,25 @@ int main() {
         std::cout << "copy[" << i << "]: " << copy[i] << std::endl;
     for (unsigned int i = 0; i < myArr.size(); i++)
         std::cout << "myArr[" << i << "]: " << myArr[i] << std::endl;
+    std::cout << std::endl;
+
+    Array<int> assign;
+    assign = Array<int>(copy);
+    for (unsigned int i = 0; i < assign.size(); i++)
+        assign[i] = -42;
+    std::cout << "assign is " << assign.size() << " elements long" << std::endl;
+    for (unsigned int i = 0; i < assign.size(); i++)
+        std::cout << "assign[" << i << "]: " << assign[i] << std::endl;
+    for (unsigned int i = 0; i < copy.size(); i++)
+        std::cout << "copy[" << i << "]: " << copy[i] << std::endl;
+    for (unsigned int i = 0; i < myArr.size(); i++)
+        std::cout << "myArr[" << i << "]: " << myArr[i] << std::endl;
+
+    try {
+        std::cout << "myArr[5]: " << myArr[5] << std::endl;
+    } catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 
     return 0;
 }

c0cfc01d0611b5958cf63b70510105d87dcf3819
rodsmade <rodrigogregoldo@gmail.com>
2023-03-10 16:36:16 -0300

    formatting

--- Cpp_07/ex02/Array.hpp
+++ Cpp_07/ex02/Array.hpp
@@ -2,31 +2,21 @@
 
 #include <cstddef>
 
-/* Example:
-template <typename T>
-class Stack {
-public:
-  Stack();
-  void push(T value);
-  T pop();
-  bool isEmpty();
-private:
-  std::vector<T> elements_;
-};
-*/
-
 template <typename T>
 class Array {
    public:
+    // Constructors/Destructor
     Array();
     Array(unsigned int size);
     Array(const Array &other);
     ~Array();
-    unsigned int size() const;
 
     // operators
     Array<T> &operator=(const Array<T> &other);
     T &operator[](unsigned int index);
+
+    // member functions
+    unsigned int size() const;
 
    private:
     T *_elements;

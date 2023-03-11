d9b4fae9c0af6d9ae97138ee0c5e2ec728d9ab3f
rodsmade <rodrigogregoldo@gmail.com>
2023-03-11 19:35:11 -0300

    improved tests and added scenario with floats

--- Cpp_07/ex00/main.cpp
+++ Cpp_07/ex00/main.cpp
@@ -3,17 +3,48 @@
 #include "whatever.hpp"
 
 int main(void) {
-    int a = 2;
-    int b = 3;
-    ::swap(a, b);
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    { // TESTING WITH INTS
+        std::cout << "======= Testing with ints: =======" << std::endl;
+        int a = 2;
+        int b = 3;
+
+        std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+
+        ::swap(a, b);
+
+        std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+        std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+        std::cout << std::endl;
+    }
+    { // TESTING WITH STRINGS
+        std::cout << "======= Testing with strings: =======" << std::endl;
+        std::string a = "chaine1";
+        std::string b = "chaine2";
+
+        std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+
+        ::swap(a, b);
+
+        std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+        std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+        std::cout << std::endl;
+    }
+    { // TESTING WITH FLOATS
+        std::cout << "======= Testing with floats: =======" << std::endl;
+        float a = 42.0f;
+        float b = -42.0f;
+
+        std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+
+        ::swap(a, b);
+
+        std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
+        std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+        std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+        std::cout << std::endl;
+    }
+
     return 0;
 }

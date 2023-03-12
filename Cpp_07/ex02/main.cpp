17456d61d0d5ae2f45ffbb3e32b6484a9d9b9b44
rodsmade <rodrigogregoldo@gmail.com>
2023-03-12 10:21:57 -0300

    improved tests

--- Cpp_07/ex02/main.cpp
+++ Cpp_07/ex02/main.cpp
@@ -1,69 +1,121 @@
+#include <cstdlib>  // for rand() and srand()
 #include <iostream>
 
 #include "Array.hpp"
 
 #define BOLD "\033[1m"
+#define LIGHT_BLUE "\033[94m"
+#define LIGHT_RED "\033[91m"
 #define ORANGE "\033[38;2;255;165;0m"
 #define VIOLET "\033[38;2;75;0;130m"
-#define DARK_GRAY "\033[90m"
-#define BG_LIGHT_YELLOW "\033[103m"
-#define LIGHT_GREEN "\033[92m"
-#define LIGHT_YELLOW "\033[93m"
-#define LIGHT_BLUE "\033[94m"
-#define LIGHT_CYAN "\033[96m"
-#define LIGHT_MAGENTA "\033[95m"
-#define LIGHT_RED "\033[91m"
 #define RESET "\033[0m"
 
-int main() {
-    // TEST 1
-    std::cout << BOLD VIOLET "TEST 1 - DEFAULT CONSTRUCTOR" RESET << std::endl;
-    Array<int> myArr(5);
+#define MAX_VAL 750  // SUBJECT TESTS
 
-    std::cout << BOLD VIOLET "| myArr is " << myArr.size() << " elements long." RESET << std::endl;
-    std::cout << BOLD VIOLET "| " RESET;
-    myArr.printAllElements();
-    std::cout << std::endl;
+int main(int, char**) {
+    {  // SUBJECT TESTS
+        std::cout << std::endl
+                  << "============= Subject Tests =============" << std::endl;
+        Array<int> numbers(MAX_VAL);
+        int* mirror = new int[MAX_VAL];
+        srand(time(NULL));
+        for (int i = 0; i < MAX_VAL; i++) {
+            const int value = rand();
+            numbers[i] = value;
+            mirror[i] = value;
+        }
+        // SCOPE
+        {
+            Array<int> tmp = numbers;
+            Array<int> test(tmp);
+        }
 
-    // TEST 2
-    std::cout << BOLD ORANGE "TEST 2 - COPY CONSTRUCTOR" RESET << std::endl;
-    Array<int> copy(myArr);
+        for (int i = 0; i < MAX_VAL; i++) {
+            if (mirror[i] != numbers[i]) {
+                std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+        }
+        try {
+            numbers[-2] = 0;
+        } catch (const std::exception& e) {
+            std::cerr << e.what() << '\n';
+        }
+        try {
+            numbers[MAX_VAL] = 0;
+        } catch (const std::exception& e) {
+            std::cerr << e.what() << '\n';
+        }
 
-    for (unsigned int i = 0; i < copy.size(); i++)
-        copy[i] = 42;
+        for (int i = 0; i < MAX_VAL; i++) {
+            numbers[i] = rand();
+        }
+        delete[] mirror;  //
+    }
+    {  // MY TESTS
+        std::cout << std::endl
+        << "=============== My Tests  ===============" << std::endl;
 
-    std::cout << BOLD ORANGE "| copy is " << copy.size() << " elements long." RESET << std::endl;
-    std::cout << BOLD ORANGE "| " RESET;
-    copy.printAllElements();
-    std::cout << BOLD ORANGE "| " RESET;
-    std::cout << BOLD VIOLET "myArr is " << myArr.size() << " elements long." RESET << std::endl;
-    std::cout << BOLD ORANGE "| " RESET;
-    myArr.printAllElements();
-    std::cout << std::endl;
+        // TEST 1
+        std::cout << BOLD VIOLET "TEST 1 - DEFAULT CONSTRUCTOR" RESET << std::endl;
+        Array<int> myArr(5);
 
-    // TEST 3
-    std::cout << BOLD LIGHT_BLUE "TEST 3 - ASSIGNMENT OPERATOR" RESET << std::endl;
-    Array<int> assign;
-    assign = Array<int>(copy);
-    for (unsigned int i = 0; i < assign.size(); i++)
-        assign[i] = -42;
-    std::cout << BOLD LIGHT_BLUE "| assign is " << assign.size() << " elements long." RESET << std::endl;
-    std::cout << BOLD LIGHT_BLUE "| " RESET;
-    assign.printAllElements();
-    std::cout << BOLD LIGHT_BLUE "| ";
-    std::cout << BOLD ORANGE "copy is " << copy.size() << " elements long." RESET << std::endl;
-    std::cout << BOLD LIGHT_BLUE "| " RESET;
-    copy.printAllElements();
-    std::cout << BOLD LIGHT_BLUE "| ";
-    std::cout << BOLD VIOLET "myArr is " << myArr.size() << " elements long." RESET << std::endl;
-    std::cout << BOLD LIGHT_BLUE "| " RESET;
-    myArr.printAllElements();
-    std::cout << std::endl;
+        std::cout << BOLD VIOLET "| `myArr' is " << myArr.size() << " elements long." RESET << std::endl;
+        std::cout << BOLD VIOLET "| " RESET;
+        myArr.printAllElements();
+        std::cout << std::endl;
 
-    try {
-        std::cout << "myArr[5]: " << myArr[5] << std::endl;
-    } catch (const std::exception &e) {
-        std::cout << LIGHT_RED BOLD << e.what() << RESET << std::endl;
+        // TEST 2
+        std::cout << BOLD ORANGE "TEST 2 - COPY CONSTRUCTOR" RESET << std::endl;
+        Array<int> copy(myArr);
+
+        for (unsigned int i = 0; i < copy.size(); i++)
+            copy[i] = 42;
+
+        std::cout << BOLD ORANGE "| `copy' is " << copy.size() << " elements long." RESET << std::endl;
+        std::cout << BOLD ORANGE "| " RESET;
+        copy.printAllElements();
+        std::cout << BOLD ORANGE "| " RESET;
+        std::cout << BOLD VIOLET "`myArr' is " << myArr.size() << " elements long." RESET << std::endl;
+        std::cout << BOLD ORANGE "| " RESET;
+        myArr.printAllElements();
+        std::cout << std::endl;
+
+        // TEST 3
+        std::cout << BOLD LIGHT_BLUE "TEST 3 - ASSIGNMENT OPERATOR" RESET << std::endl;
+        Array<int> assign;
+        assign = Array<int>(copy);
+        for (unsigned int i = 0; i < assign.size(); i++)
+            assign[i] = -42;
+        std::cout << BOLD LIGHT_BLUE "| `assign' is " << assign.size() << " elements long." RESET << std::endl;
+        std::cout << BOLD LIGHT_BLUE "| " RESET;
+        assign.printAllElements();
+        std::cout << BOLD LIGHT_BLUE "| ";
+        std::cout << BOLD ORANGE "`copy' is " << copy.size() << " elements long." RESET << std::endl;
+        std::cout << BOLD LIGHT_BLUE "| " RESET;
+        copy.printAllElements();
+        std::cout << BOLD LIGHT_BLUE "| ";
+        std::cout << BOLD VIOLET "`myArr' is " << myArr.size() << " elements long." RESET << std::endl;
+        std::cout << BOLD LIGHT_BLUE "| " RESET;
+        myArr.printAllElements();
+        std::cout << std::endl;
+
+        std::cout << BOLD LIGHT_RED "TEST 4 - ACCESSING WRONG INDEXES" RESET << std::endl;
+        try {
+            std::cout << "myArr[5]: " << myArr[5] << std::endl;
+        } catch (const std::exception& e) {
+            std::cout << LIGHT_RED BOLD << e.what() << RESET << std::endl;
+        }
+        try {
+            std::cout << "myArr[-1]: " << myArr[-1] << std::endl;
+        } catch (const std::exception& e) {
+            std::cout << LIGHT_RED BOLD << e.what() << RESET << std::endl;
+        }
+        try {
+            std::cout << "myArr[2147483647]: " << myArr[2147483647] << std::endl;
+        } catch (const std::exception& e) {
+            std::cout << LIGHT_RED BOLD << e.what() << RESET << std::endl;
+        }
     }
 
     return 0;

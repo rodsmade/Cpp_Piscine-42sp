24c3e52f98a67cae09833a406f0f743ae2d7a3a2
rodsmade <rodrigogregoldo@gmail.com>
2023-03-13 19:49:24 -0300

    added subject tests

--- Cpp_08/ex02/main.cpp
+++ Cpp_08/ex02/main.cpp
@@ -2,6 +2,32 @@
 
 #include "MutantStack.hpp"
 
+// int main()
+// {
+//     MutantStack<int> mstack;
+//     mstack.push(5);
+//     mstack.push(17);
+//     std::cout << mstack.top() << std::endl;
+//     mstack.pop();
+//     std::cout << mstack.size() << std::endl;
+//     mstack.push(3);
+//     mstack.push(5);
+//     mstack.push(737);
+//     //[...]
+//     mstack.push(0);
+//     MutantStack<int>::iterator it = mstack.begin();
+//     MutantStack<int>::iterator ite = mstack.end();
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+//     std::stack<int> s(mstack);
+//     return 0;
+// }
+
 void reportStackSize(const MutantStack<int>& s)
 {
     std::cout << s.size() << " elements on stack\n";
@@ -43,6 +69,4 @@ int main()
     for (int index = 0; index < (ite - it); index++){
         std::cout << "Pos [" << index << "] Value[" << *(it + index) << "]" << std::endl;
     }
-
-
 }

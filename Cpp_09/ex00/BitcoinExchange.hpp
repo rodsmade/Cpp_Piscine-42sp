865831a4cec0ebdfe9bc7caefa5a6e0f819c2904
rodsmade <rodrigogregoldo@gmail.com>
2023-07-15 16:07:56 -0300

    initial problem modeling

--- Cpp_09/ex00/BitcoinExchange.hpp
+++ Cpp_09/ex00/BitcoinExchange.hpp
@@ -1,6 +1,36 @@
 #ifndef BITCOIN_EXCHANGE_HPP
 #define BITCOIN_EXCHANGE_HPP
 
+#include <cstdio>
 #include <iostream>
+#include <map>
+
+class Date {
+   private:
+    int day;
+    int month;
+    int year;
+
+   public:
+    Date(std::string dateStr);
+    ~Date();
+    Date(const Date &other);
+    Date &operator=(const Date &other);
+
+    // implement operators > and ==, probably 
+};
+
+class BitcoinExchange {
+   private:
+    std::map<Date, float> database;
+
+   public:
+    BitcoinExchange(std::FILE* data); // loads .csv into map for faster lookup
+    ~BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange &other);
+    BitcoinExchange &operator=(const BitcoinExchange &other);
+
+    Date floor(const Date &date); // returns the date inside `database` equal to or immediately smaller than `date`
+};
 
 #endif

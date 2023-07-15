d1732b55c031173c4025d3b866e6b239a46a81af
rodsmade <rodrigogregoldo@gmail.com>
2023-07-15 15:01:48 -0300

    basic input validation structure

--- Cpp_09/ex00/main.cpp
+++ Cpp_09/ex00/main.cpp
@@ -1,6 +1,19 @@
+#include <cstdlib>
+
 #include "BitcoinExchange.hpp"
 
-int main() {
+void    validate_args(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Error: could not open file." << std::endl;
+        std::exit(-1);
+    }
+    (void) argv;
+}
+
+int main(int argc, char **argv) {
     std::cout << "HELLO HELLO, HE, HELLO, HELLO" << std::endl;
+    validate_args(argc, argv);
+    
     return 0;
 }
+

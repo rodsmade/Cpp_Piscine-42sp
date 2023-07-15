b0702a034d2f99a8059f410b0cde928084342c2a
rodsmade <rodrigogregoldo@gmail.com>
2023-07-15 16:05:27 -0300

    basic input validation: ✅

--- Cpp_09/ex00/main.cpp
+++ Cpp_09/ex00/main.cpp
@@ -1,19 +1,33 @@
-#include <cstdlib>
+#include <cstdlib>  // exit()
 
 #include "BitcoinExchange.hpp"
 
 void    validate_args(int argc, char **argv) {
     if (argc != 2) {
         std::cout << "Error: could not open file." << std::endl;
+    }
+    std::FILE* file = std::fopen(argv[1], "r");
+    if (file == NULL) {
+        std::cout << "Error: could not open file." << std::endl;
         std::exit(-1);
     }
-    (void) argv;
+
+    std::fclose(file);
+    std::cout << "File exists and has read permission. Now do sth with it." << std::endl;
 }
+
+// void    load_database() {
+//     const char* filename = "cpp_09/data.csv";
+//     std::FILE* file = std::fopen(filename, "r");
+
+//     BitcoinExchange database(file);
+// }
 
 int main(int argc, char **argv) {
     std::cout << "HELLO HELLO, HE, HELLO, HELLO" << std::endl;
     validate_args(argc, argv);
-    
+
+    // load_database();
     return 0;
 }
 

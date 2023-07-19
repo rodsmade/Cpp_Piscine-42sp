17f12a2bd179504233bce757929dbe723da726ed
rodsmade <rodrigogregoldo@gmail.com>
2023-07-19 17:43:15 -0300

    initial project structure

---
+++ Cpp_09/ex02/PmergeMe.cpp
@@ -0,0 +1,9 @@
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {};
+
+PmergeMe::~PmergeMe() {};
+
+PmergeMe::PmergeMe(const PmergeMe &other) { (void) other; };
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &other) { (void) other; return *this; };
---
+++ Cpp_09/ex02/PmergeMe.hpp
@@ -0,0 +1,18 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <vector>
+#include <deque>
+
+class PmergeMe {
+   public:
+    PmergeMe();
+    ~PmergeMe();
+    PmergeMe(const PmergeMe &other);
+    PmergeMe &operator=(const PmergeMe &other);
+
+   private:
+    // vector e deque?
+};
+
+#endif

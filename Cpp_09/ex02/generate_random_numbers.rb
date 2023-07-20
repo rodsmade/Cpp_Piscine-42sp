47ba163c78bd814641f6f582b74b991118ef95c1
rodsmade <rodrigogregoldo@gmail.com>
2023-07-20 19:04:52 -0300

    adding random number generator to project

---
+++ Cpp_09/ex02/generate_random_numbers.rb
@@ -0,0 +1,17 @@
+# Replace x with the desired number of unique random numbers you want to generate
+x = 3000
+
+# Replace range_start and range_end with the desired range for random numbers
+range_start = 0
+range_end = 30000
+
+# Generate an array of numbers from range_start to range_end
+sequence = (range_start..range_end).to_a
+
+# Shuffle the array to get random order of elements
+shuffled_sequence = sequence.shuffle
+
+# Select the first x numbers from the shuffled array to get unique random numbers
+unique_random_numbers = shuffled_sequence.take(x)
+
+puts unique_random_numbers.join(' ')
